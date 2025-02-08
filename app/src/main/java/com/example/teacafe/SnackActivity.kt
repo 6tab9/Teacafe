3d5dd1ac6d2134bbc8d8d644e498d40b383235cf
6tab9 <tabolator1@gmail.com>
2025-02-08 19:03:29 +0100

    yes

--- app/src/main/java/com/example/teacafe/SnackActivity.kt
+++ app/src/main/java/com/example/teacafe/SnackActivity.kt
@@ -1,20 +1,30 @@
 package com.example.teacafe
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class SnackActivity : AppCompatActivity() {
+    private lateinit var Image: ImageView
+    private lateinit var Title: TextView
+    private lateinit var Descr: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_snack)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val SnackId = intent.getIntExtra("snackID",-1)
+        val Snack = Snack.snacks[SnackId]
+
+        Image = findViewById(R.id.Image)
+        Title = findViewById(R.id.name)
+        Descr = findViewById(R.id.descr)
+        Image.setImageResource(Snack.image)
+        Image.setColorFilter(Random.nextInt()*1000)
+        Title.text = Snack.name
+        Descr.text = Snack.description
     }
 }
--- app/src/main/java/com/example/teacafe/SnackCategoryActivity.kt
+++ app/src/main/java/com/example/teacafe/SnackCategoryActivity.kt
@@ -1,20 +1,32 @@
 package com.example.teacafe
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SnackCategoryActivity : AppCompatActivity() {
+    private lateinit var snackListView: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_snack_category)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        snackListView = findViewById(R.id.snackListView)
+
+        val drinkArrayAdapter = ArrayAdapter(
+            this,android.R.layout.simple_list_item_1,Snack.snacks
+        )
+
+        snackListView.adapter = drinkArrayAdapter
+
+        snackListView.setOnItemClickListener{_,_,position,_->
+            startActivity(
+                Intent(this,SnackActivity::class.java).putExtra("snackID",position)
+            )
         }
     }
 }
